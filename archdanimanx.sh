4d7f59e2f2c344719b5ed9b05eb3570fb2e1aeb3
DanimanX <70829086+DanimanX@users.noreply.github.com>
2023-07-15 20:22:20 -0400

    Add files via upload

---
+++ archdanimanx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
+CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+set +a
+echo -ne "
+-------------------------------------------------------------------------
+ _______                       __                                    __    __
+|       \                     |  \                                  |  \  |  \
+| $$$$$$$\  ______   _______   \$$ ______ ____    ______   _______  | $$  | $$
+| $$  | $$ |      \ |       \ |  \|      \    \  |      \ |       \  \$$\/  $$
+| $$  | $$  \$$$$$$\| $$$$$$$\| $$| $$$$$$\$$$$\  \$$$$$$\| $$$$$$$\  >$$  $$
+| $$  | $$ /      $$| $$  | $$| $$| $$ | $$ | $$ /      $$| $$  | $$ /  $$$$\
+| $$__/ $$|  $$$$$$$| $$  | $$| $$| $$ | $$ | $$|  $$$$$$$| $$  | $$|  $$ \$$\
+| $$    $$ \$$    $$| $$  | $$| $$| $$ | $$ | $$ \$$    $$| $$  | $$| $$  | $$
+ \$$$$$$$   \$$$$$$$ \$$   \$$ \$$ \$$  \$$  \$$  \$$$$$$$ \$$   \$$ \$$   \$$
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+-------------------------------------------------------------------------
+                Scripts are in directory named ArchDanimanX
+"
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+      source $CONFIGS_DIR/setup.conf
+    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt $HOME/ArchDanimanX/scripts/1-setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+    fi
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    cp -v *.log /mnt/home/$USERNAME
+
+echo -ne "
+-------------------------------------------------------------------------
+ _______                       __                                    __    __
+|       \                     |  \                                  |  \  |  \
+| $$$$$$$\  ______   _______   \$$ ______ ____    ______   _______  | $$  | $$
+| $$  | $$ |      \ |       \ |  \|      \    \  |      \ |       \  \$$\/  $$
+| $$  | $$  \$$$$$$\| $$$$$$$\| $$| $$$$$$\$$$$\  \$$$$$$\| $$$$$$$\  >$$  $$
+| $$  | $$ /      $$| $$  | $$| $$| $$ | $$ | $$ /      $$| $$  | $$ /  $$$$\
+| $$__/ $$|  $$$$$$$| $$  | $$| $$| $$ | $$ | $$|  $$$$$$$| $$  | $$|  $$ \$$\
+| $$    $$ \$$    $$| $$  | $$| $$| $$ | $$ | $$ \$$    $$| $$  | $$| $$  | $$
+ \$$$$$$$   \$$$$$$$ \$$   \$$ \$$ \$$  \$$  \$$  \$$$$$$$ \$$   \$$ \$$   \$$
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+-------------------------------------------------------------------------
+                Done - Please Eject Install Media and Reboot
+"
